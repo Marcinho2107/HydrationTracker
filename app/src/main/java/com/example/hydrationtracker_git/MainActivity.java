32a03bc2106d0ea790f96c4f1b1e5bcb080a3495
Niklind Haxhija <niklindhaxhia@gmail.com>
2024-05-05 20:52:13 +0200

    New LogIN Button und MainScren Nik

--- app/src/main/java/com/example/hydrationtracker_git/MainActivity.java
+++ app/src/main/java/com/example/hydrationtracker_git/MainActivity.java
@@ -1,20 +1,23 @@
 package com.example.hydrationtracker_git;
 
-import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    Button logInButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logInButton=findViewById(R.id.Button);
+        logInButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this,MainScreen.class);
+            startActivity(intent);
+        });
     }
-    
-    public static void TUTNIX2222 (int Number){
-
-        //test methode
-    }
-
-
 }
