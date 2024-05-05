38e1fe3dc3bcc303ede68f04a5a16f8b482f27d0
Marco Krauss <kraussiqsg@gmail.com>
2024-05-05 13:08:52 +0200

    test

--- app/src/main/java/com/example/hydrationtracker_git/MainActivity.java
+++ app/src/main/java/com/example/hydrationtracker_git/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     
-    public static void TUTNIX (){
+    public static void TUTNIX2222 (int Number){
 
         //test methode
     }
