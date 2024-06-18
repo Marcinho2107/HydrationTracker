acbb026127ae83d6c08a8225ccb89928c6e1ce29
Niklind Haxhija <niklindhaxhia@gmail.com>
2024-06-18 02:29:01 +0200

    Revert "Main Screen V1.1_5"
    This reverts commit e4f0d974

--- app/src/main/java/com/example/hydrationtracker_git/MainScreen.java
+++ app/src/main/java/com/example/hydrationtracker_git/MainScreen.java
@@ -1,24 +1,13 @@
 package com.example.hydrationtracker_git;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
@@ -29,14 +18,10 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
-import java.util.List;
-import java.util.Locale;
 
 /** @noinspection ALL*/
-public class MainScreen extends AppCompatActivity implements LocationListener, NavigationView.OnNavigationItemSelectedListener {
-    TextView txt_view_location;
-    Button button_location;
-    LocationManager locationManager_location;
+public class MainScreen extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
     DrawerLayout dl;
     BottomNavigationView bnv;
     FragmentManager fm;
@@ -47,12 +32,9 @@ public class MainScreen extends AppCompatActivity implements LocationListener, N
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
-        //Initializing Location Items
-        //txt_view_location = findViewById(R.id.location_txt_view);
-        //button_location = findViewById(R.id.location_button);
         //Initializing Main Screen Navigation Items
-        fab = findViewById(R.id.floatingActionButton);
-        t = findViewById(R.id.toolbar);
+        fab = findViewById(R.id.floatingActionButton_bottom);
+        t = findViewById(R.id.toolbar_top);
         setSupportActionBar(t);
         dl = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, dl, t, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -60,7 +42,7 @@ public class MainScreen extends AppCompatActivity implements LocationListener, N
         toggle.syncState();
         NavigationView navigationView = findViewById(R.id.navigationView);
         navigationView.setNavigationItemSelectedListener(this);
-        bnv = findViewById(R.id.bottomNavigationView);
+        bnv = findViewById(R.id.bottomNavigationView_bottom);
         bnv.setBackground(null);
         bnv.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -84,14 +66,6 @@ public class MainScreen extends AppCompatActivity implements LocationListener, N
                 Toast.makeText(MainScreen.this,"Upload", Toast.LENGTH_SHORT).show();
             }
         }));
-        //Runtime Permission
-        if (ContextCompat.checkSelfPermission(MainScreen.this, Manifest.permission.ACCESS_FINE_LOCATION)
-        != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(MainScreen.this, new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION
-            },100);
-        }
-        button_location.setOnClickListener(v -> getLocation());
         //WeatherAPIRequest Instance
         WeatherAPIRequest weatherAPIRequest = new WeatherAPIRequest();
         //WeatherAPIRequest Instance URL()
@@ -125,45 +99,9 @@ public class MainScreen extends AppCompatActivity implements LocationListener, N
     }
     private void openFragment(Fragment fragment){
         FragmentTransaction transaction=fm.beginTransaction();
-        transaction.replace(R.id.fragment_layout_fragmentContainer, fragment);
+        transaction.replace(R.id.frameLayout_all, fragment);
         transaction.commit();
     }
-    //Location Methods
-    @SuppressLint("MissingPermission")
-    private void getLocation(){
-        try{
-            locationManager_location = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
-            locationManager_location.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, MainScreen.this);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-    @Override
-    public void onLocationChanged(@NonNull Location location) {
-        Toast.makeText(this, location.getLatitude()+","+location.getLongitude(), Toast.LENGTH_SHORT).show();
-        try{
-            Geocoder geocoder = new Geocoder(MainScreen.this, Locale.getDefault());
-            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
-            assert addresses != null;
-            String address = addresses.get(0).getAddressLine(0);
-            txt_view_location.setText(address);
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        LocationListener.super.onStatusChanged(provider, status, extras);
-    }
-    @Override
-    public void onProviderEnabled(@NonNull String provider) {
-        LocationListener.super.onProviderEnabled(provider);
-    }
-    @Override
-    public void onProviderDisabled(@NonNull String provider) {
-        LocationListener.super.onProviderDisabled(provider);
-    }
-
     public void radio_button_clicked(View view) {
     }
 }
